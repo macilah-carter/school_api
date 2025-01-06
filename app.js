b8deb5b01b16ebe5da3243f5ec847eaf4129f070
macilah-carter <museemacilah@gmail.com>
2025-01-06 15:18:33 +0300

    vercel.json file

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //loggin and signup
-app.use('/student', require('./routes/student/auth'));
-app.use('/admin', require('./routes/admin/authAdmin'));
+app.use('/student/login', require('./routes/student/auth'));
+app.use('/admin/signup', require('./routes/admin/authAdmin'));
 
 //crud
-app.use('/',verify, require('./routes/student/student'));
-app.use('/',verifyAdmin, require('./routes/admin/admin'));
+app.use('/student',verify, require('./routes/student/student'));
+app.use('/admin',verifyAdmin, require('./routes/admin/admin'));
 
 app.get('/',(req,res)=>{
     res.send("Hello, This is the home page")
